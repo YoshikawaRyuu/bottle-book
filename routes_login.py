ddc0a56a97639c7716fec0401add0018f19fb553
YoshikawaRyuu <yoshiryu.pc0110@gmail.com>
2023-10-24 12:48:29 +0900

    更新

--- routes_login.py
+++ routes_login.py
@@ -28,3 +28,31 @@ def index():
         #pass
         #err_msg = urlpar.unquote(err_msg)
     return template('login.html', error=err_msg)
+
+@app.route('/login', method='POST')
+def login():
+    #ログイン処理、POSTのみ受付
+    user_id = request.forms.decode().get('user_id')
+    passwd = request.forms.decode().get('passwd')
+    
+    user = connection.query(BookUser.user_id).filter\
+        (BookUser.user_id == user_id,\
+            BookUser.passwd == passwd,\
+                BookUser.delFlg == False).scalar()
+    print(user)
+    if user is not None:
+        #認証成功
+        auth = Auth()
+        auth.add_auth(user)
+        redirect(REDIRECT_AFTER_LOGIN)
+    else:
+        #認証失敗
+        err_msg = urlpar.quote('認証に失敗しました')
+        redirect(REDIRECT_AFTER_LOGOFF + '?error' + err_msg)
+        
+@app.route('/logout', method=['GET','POST'])
+def logout():
+    #ログオフ
+    auth = Auth()
+    auth.del_auth()
+    redirect(REDIRECT_AFTER_LOGOFF)
