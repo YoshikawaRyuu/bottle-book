530763d3f6c016711a7005426b03bae5489a1748
YoshikawaRyuu <yoshiryu.pc0110@gmail.com>
2023-10-24 20:59:08 +0900

    途中までの更新

--- routes_form.py
+++ routes_form.py
@@ -34,3 +34,67 @@ def add():
             form['memo'] = book.memo
             registId = book.id_
             
+            kind = "編集"
+        #フォーム画面表示処理
+        return template('add.html'
+                , form=form
+                , kind=kind
+                , registId=registId)
+    #POSTされた場合
+    elif request.method == 'POST':
+        #POST値の取得
+        form['name'] = request.forms.decode().get('name')
+        form['volume'] = request.forms.decode().get('volume')
+        form['author'] = request.forms.decode().get('author')
+        form['publisher'] = request.forms.decode().get('publisher')
+        form['memo'] = request.forms.decode().get('memo')
+        registId = ""
+        #idが指定されている場合
+        if request.forms.decode().get('id') is not None:
+            registId = request.forms.decode().get('id')
+            
+        #バリデーション処理
+        errorMsg = Utils.validate(data=form)
+        #コンソールに表示処理
+        print(errorMsg)
+        if request.forms.get('next') == 'back':
+            return template('add.html'
+                    , form=form
+                    , kind=kind
+                    , registId=registId)
+
+        if errorMsg == []:
+            #入力エラーが無いと確認画面の表示
+            headers = ['著書名','巻数','著作者','出版社','メモ']
+            return template('confirm.html'
+                    , form=form
+                    , headers=headers
+                    , registId=registId)
+        else:
+            #入力エラーがあると再度入力画面を表示
+            return template('add.html'
+                    , error=errorMsg
+                    , kind=kind
+                    , form=form
+                    , registId=registId)
+            
+#確認画面から登録画面にPOSTされる
+@app.route('/regist', method='POST')
+def regist():
+    #認証確認
+    auth.check_login()
+    #データ受取
+    name = request.forms.get('name')
+    volume = request.forms.get('volume')
+    author = request.forms.get('author')
+    publisher = request.forms.get('publisher')
+    memo = request.forms.get('memo')
+    registId = request.forms.get('id')
+
+    if request.forms.get('next') == 'back':
+        #確認画面から戻るボタンを押す
+        #登録フォームに戻る
+        response.status = 307
+        response.set_header("Location", '/add')
+        return response
+    
